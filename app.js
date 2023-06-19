b0a669377052702cdb1cd22112e373d47e001794
Ahmet Can Menemencioğlu <95551059+ahmtcnmn@users.noreply.github.com>
2023-06-19 13:52:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.use("/src", express.static("src"));
 import pages from "./router/pages.js";
 //---------------------------------------------------------------------------------------------------------------------------------------
 
-app.get("/", pages);
-app.get("/iletisim", pages);
-app.get("/iletisim/:id", pages);
-app.post("/iletisim/:id", pages);
-app.get("/iletisim/delete/:id", pages);
-app.get("/addnew", pages);
-app.post("/addnew/new", pages);
-app.get('/update', pages)
+app.use("/", pages);
+app.use("/iletisim", pages);
+app.use("/iletisim/:id", pages);
+app.use("/iletisim/:id", pages);
+app.use("/iletisim/delete/:id", pages);
+app.use("/addnew", pages);
+app.use("/addnew/new", pages);
+app.use('/update', pages)
 
 
 
